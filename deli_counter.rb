450cc13ce4cd218fa2e561f8b6642fcc47bba6b7
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-21 15:46:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+katz_deli = []
+
+def line(x)
+  line = []
+  if x.length == 0
+    puts "The line is currently empty."
+  else 
+    x.each_with_index do |name, index|
+    line.push("#{index+1}. #{name}")
+  end
+    puts "The line is currently: #{line.join(" ")}"
+  end
+end
+
+def take_a_number(x, name)
+  x.push
+  puts "Welcome, #{name}. You are number #{x.length} in line."
+end
