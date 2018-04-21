ed6fd6a45606f7cfcf49981c014ae2d6ac3814c5
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-21 15:49:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,8 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    
+    puts "Currently serving #{name}."
+    katz_deli
   end
 end
     
