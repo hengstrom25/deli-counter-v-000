6bd1a142846c5d10239d9ffd1630ce4cf4c7c48a
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-21 15:46:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def line(x)
   end
 end
 
-def take_a_number(x, name)
-  x.push
-  puts "Welcome, #{name}. You are number #{x.length} in line."
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
